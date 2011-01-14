bf185537f3f593a94abc330b47cb949b39141698
Jérémie Laval <jeremie.laval@gmail.com>
2011-01-14 17:39:16 +0000

    Better version of StaticContentModule

--- StaticContentModule.cs
+++ StaticContentModule.cs
@@ -30,12 +30,14 @@ namespace Apachai
 {
 	public class StaticContentModule : ManosModule
 	{
-		readonly IEnumerable<string> cacheExtensions;
+		readonly HashSet<string> cacheExtensions;
 		readonly string expires;
+		readonly string cacheControl;
 		readonly Dictionary<string, string> etagCache;
 		readonly FileSystemWatcher fsw;
 
-		static readonly string[] defaultCachedExts = { ".js", ".css", ".png", ".jpg", ".woff", ".eot", ".ttf", ".ico" };
+		static readonly string[] defaultCachedExts = { ".js", ".css", ".png", ".jpg", ".woff",
+		                                               ".eot", ".ttf", ".ico" };
 
 		public StaticContentModule () : this (null)
 		{
@@ -49,22 +51,28 @@ namespace Apachai
 		                            IEnumerable<string> cacheExtensions,
 		                            TimeSpan expireTime)
 		{
-			if (!string.IsNullOrEmpty (dir)) {
-				this.cacheExtensions = cacheExtensions;
-				this.expires = (DateTime.Now + TimeSpan.FromDays (30)).ToString ("R");
-				this.etagCache = new Dictionary<string, string> ();
+			if (string.IsNullOrEmpty (dir))
+				return;
+			if (dir.StartsWith (Path.PathSeparator.ToString ()))
+				dir = dir.Substring (1);
 
-				if (dir.StartsWith (Path.PathSeparator.ToString ()))
-					dir = dir.Substring (1);
+			this.cacheExtensions = new HashSet<string> (cacheExtensions);
+			this.expires = (DateTime.Now + expireTime).ToString ("R");
+			this.cacheControl = "max-age=" + ((int)expireTime.TotalSeconds).ToString ();
+			this.etagCache = new Dictionary<string, string> ();
 
-				fsw = new FileSystemWatcher (dir);
-				fsw.NotifyFilter = NotifyFilters.LastWrite;
-				fsw.IncludeSubdirectories = true;
-				fsw.Changed += (obj, e) => etagCache[Path.Combine (dir, e.Name)] = GetEtagFromFile (e.FullPath);
-				fsw.EnableRaisingEvents = true;
-			}
+			fsw = new FileSystemWatcher (dir);
+			InitFileSystemWatcher (dir);
 
 			Get (".*", Content);
+		}
+
+		void InitFileSystemWatcher (string dir)
+		{
+			fsw.NotifyFilter = NotifyFilters.LastWrite;
+			fsw.IncludeSubdirectories = true;
+			fsw.Changed += (obj, e) => etagCache[Path.Combine (dir, e.Name)] = GetEtagFromFile (e.FullPath);
+			fsw.EnableRaisingEvents = true;
 		}
 
 		public void Content (IManosContext ctx)
@@ -89,16 +97,13 @@ namespace Apachai
 				ctx.Response.Headers.SetNormalizedHeader ("Content-Type", ManosMimeTypes.GetMimeType (path));
 
 				// Expires setting for specific files
-				if (cacheExtensions != null) {
-					foreach (var extension in cacheExtensions) {
-						if (path.EndsWith (extension, StringComparison.Ordinal)) {
-							ctx.Response.Headers.SetNormalizedHeader ("Expires", expires);
-							if (!etagCache.ContainsKey (path))
-								etagCache[path] = GetEtagFromFile (path);
-							ctx.Response.Headers.SetNormalizedHeader ("ETag", etagCache[path]);
-							break;
-						}
-					}
+				string extension = Path.GetExtension (path);
+				if (cacheExtensions != null && !string.IsNullOrEmpty (extension) && cacheExtensions.Contains (extension)) {
+					ctx.Response.Headers.SetNormalizedHeader ("Expires", expires);
+					ctx.Response.Headers.SetNormalizedHeader ("Cache-Control", cacheControl);
+					if (!etagCache.ContainsKey (path))
+						etagCache[path] = GetEtagFromFile (path);
+					ctx.Response.Headers.SetNormalizedHeader ("ETag", etagCache[path]);
 				}
 
 				ctx.Response.SendFile (path);
