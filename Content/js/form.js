075dafb40d2ac4bc527ab5f0f399eb1bcceaceef
Jérémie Laval <jeremie.laval@gmail.com>
2011-07-19 18:12:10 +0200

    Unwrap posting js

--- Content/js/form.js
+++ Content/js/form.js
@@ -2,50 +2,48 @@
    
 */
 
-(function($){
-	if (document.cookie.indexOf ('apachai') == -1)
-		window.location.replace ('/Login');
+if (document.cookie.indexOf ('apachai') == -1)
+	window.location.replace ('/Login');
 
-	var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
 
-	if ($.inArray ("error=1", hashes) != -1) {
-		$("#invalid").removeClass("hidden");
+if ($.inArray ("error=1", hashes) != -1) {
+	$("#invalid").removeClass("hidden");
+}
+
+function onFileChange (value) {
+	if (!value.toLowerCase ().match (".*\.(jpg|jpeg)")) {
+		$("#invalid").removeClass ("hidden");
+		$("#submit").attr ("disabled", "disabled");
+	} else {
+		$("#invalid").addClass ("hidden");
+		$("#submit").removeAttr ("disabled");
 	}
+}
 
-	function onFileChange (value) {
-		if (!value.toLowerCase ().match (".*\.(jpg|jpeg)")) {
-			$("#invalid").removeClass ("hidden");
-			$("#submit").attr ("disabled", "disabled");
-		} else {
-			$("#invalid").addClass ("hidden");
-			$("#submit").removeAttr ("disabled");
-		}
+function onTwitterChange (value) {
+	var left = 100 - value.length;
+	var elem = $("#numCharacters");
+	elem.text (left);
+
+	if (left < 0) {
+		elem.addClass ("numWrong");
+		elem.removeClass ("numRight");
+		$("#submit").attr ("disabled", "disabled");
+	} else {
+		elem.addClass ("numRight");
+		elem.removeClass ("numWrong");
+		$("#submit").removeAttr ("disabled");
 	}
+}
 
-	function onTwitterChange (value) {
-		var left = 100 - value.length;
-		var elem = $("#numCharacters");
-		elem.text (left);
+$('#effect').change (function () {
+	var srcs = { "eff_original" : "cat.jpg",
+				 "eff_sepia" : "cat_sepia.jpg",
+				 "eff_invert" : "cat_invert.jpg",
+				 "eff_blackwhite" : "cat_bw.jpg"};
+	var value = $('#effect').val();
 
-		if (left < 0) {
-			elem.addClass ("numWrong");
-			elem.removeClass ("numRight");
-			$("#submit").attr ("disabled", "disabled");
-		} else {
-			elem.addClass ("numRight");
-			elem.removeClass ("numWrong");
-			$("#submit").removeAttr ("disabled");
-		}
-	}
-
-	$('#effect').change (function () {
-		var srcs = { "eff_original" : "cat.jpg",
-					 "eff_sepia" : "cat_sepia.jpg",
-					 "eff_invert" : "cat_invert.jpg",
-					 "eff_blackwhite" : "cat_bw.jpg"};
-		var value = $('#effect').val();
-
-		if (value in srcs)
-			$('#img_preview img').attr('src', "/Content/img/" + srcs[value]);
-	});
-})(window.jQuery);
+	if (value in srcs)
+		$('#img_preview img').attr('src', "/Content/img/" + srcs[value]);
+});
